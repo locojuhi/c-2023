5e986a85666d907b8a37e3b541c48c78c5dc3404
Ivan Garcerant <igarcerant@gmail.com>
2023-01-20 18:40:46 +0000

    first algorithm - a trivial one

---
+++ swap/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(int argc,char** argv)
+{
+    int a = 1;
+    int b = 2;
+    //swap(a,b);
+    //int c = b; b = a; a = c;
+    printf("%d %d\n", a, b);
+    b = b + a;
+    a = b - a;
+    b = b - a;
+    printf("%d %d\n", a, b);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
